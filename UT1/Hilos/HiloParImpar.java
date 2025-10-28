90f5d107d8b6a0804a43d45f901591c7121c94d6
DiegoBallesteros <diego.ball.fdz@gmail.com>
2025-10-28 10:06:16 +0100

    correcion final hilo1

--- UT1/Hilos/HiloParImpar.java
+++ UT1/Hilos/HiloParImpar.java
@@ -7,13 +7,13 @@ public class HiloParImpar implements Runnable{
     public void run(){
         int num=0;
         do{
-        num++;
+            num++;
     if(tipo==1){//filtramos por par
         if (Thread.currentThread().getName()=="HILO 1" && num<=100){//filtramos por hilo 1
                 System.out.println(Thread.currentThread().getName()+" generando número par"+num);
         }
     }else{//filtramos por impar
-        if (Thread.currentThread().getName()=="HILO 2" && num>100){//filtramos por hilo 2
+        if (Thread.currentThread().getName()=="HILO 2" && num>100){//filtramos por hilo 2   
                 System.out.println(Thread.currentThread().getName()+" generando número impar"+num);
         }
     }
--- UT1/Hilos/HiloParImparEjecutable.java
+++ UT1/Hilos/HiloParImparEjecutable.java
@@ -1,8 +1,13 @@
 
 public class HiloParImparEjecutable {
-    public static void main(String[] args) {
-        new Thread(new HiloParImpar(1),"HILO 1").start();
-        new Thread(new HiloParImpar(2),"HILO 2").start();
+    public static void main(String[] args) throws InterruptedException {
+        Thread h1=new Thread(new HiloParImpar(1),"HILO 1");
+        Thread h2=new Thread(new HiloParImpar(2),"HILO 2");
+        h2.start();
+        h2.join();
+        h1.start();
+        //System.out.println("Finaliza el programa");
+
     }
     
 }
