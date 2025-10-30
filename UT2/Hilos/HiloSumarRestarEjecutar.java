5d289d598b3b67eaa6cb1e70c3304703b17ace60
DiegoBallesteros <diego.ball.fdz@gmail.com>
2025-10-30 12:22:36 +0100

    corrección sumasrestas

--- UT2/Hilos/HiloSumarRestarEjecutar.java
+++ UT2/Hilos/HiloSumarRestarEjecutar.java
@@ -5,15 +5,12 @@ public class HiloSumarRestarEjecutar {
         Thread h3=new Thread(new HiloSumasRestas(1,"+"),"HiloSumar3");
         Thread h4=new Thread(new HiloSumasRestas(1,"-"),"HiloRestar4");
 
-        
+        //para que funcione como queremos debemos omitir el uso del ultimo hilo y gestiar la ejecución
+
         h4.start();
         h4.join();
         h3.start();
         h3.join();
         h2.start();
-
-        
-
-
     }
 }
