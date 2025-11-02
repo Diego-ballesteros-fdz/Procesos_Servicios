06115fa50a518c9b72431197815e412ba19f4ff8
DiegoBallesteros <diego.ball.fdz@gmail.com>
2025-11-02 11:57:23 +0100

    Preparacion examen

--- UT1/SimulacroExam/simulacroGPT1.c
+++ UT1/SimulacroExam/simulacroGPT1.c
@@ -5,25 +5,50 @@
 #include <sys/stat.h>
 #include <signal.h>
 #include <sys/wait.h>
+#include <time.h>
+
+
+//variables globales
+pid_t p2,p3;//pid para los hijos.
 
 //Funcion manejadora de las señales
 void fun_signal(int signal){
-	
 	//capturaremos la señal sigint
-	
-	
-	
-	
+	if(signal==SIGINT){//PARA ACABAR PROCESOS
+		if(p2!=0 && p3!=0){//proceso p1
+			//mandamos la señal a p2 y p3
+			kill(p2,SIGINT);
+			kill(p3,SIGINT);
+			//esperamso a que terminen los hijos
+			wait(NULL);
+			wait(NULL);
+			//salimos
+			printf("deteniendo proceso p1\n");
+			exit(0);
+		}else{
+			if(p2==0){
+				printf("deteniendo proceso p2\n");
+				exit(0);
+			}else{
+				printf("deteniendo proceso p3\n");
+				exit(0);
+			}
+		}
 	}
+}
+
 
 
 //metodo main
-int main(){
+void main(){
 	//declaración de variables
-	pid_t p2,p3;//pid para los hijos.
-	int df1[2],df2[2]//descriptor para los pipe anonimos.
+	int df1[2],df2[2];//descriptor para los pipe anonimos.
 	int num;//numero que debemos pasar
+	char numero[30];//numero en String
 	char buffer[30];//buffer de almacenaje del pipe
+	//variables para el random
+	time_t t;
+	srand((unsigned) time(&t));
 	
 	// Esta línea registra la función manejadora para la señal SIGINT. 
 	//A partir de aquí, si se pulsa Ctrl+C, se ejecutará fun_signal(), en todos los procesos.
@@ -35,31 +60,79 @@ int main(){
 	//creamos el primer hijo
 	p2=fork();
 	
-	if(p2!=0){//proceso p1
+	if(p2==0){//proceso p2
+	
+		//creamos el numero aleatorio para enviarlo, maximo 10 minimo 1
+		num=rand() % 10 +1;
+		
+		//convertimos a string
+		sprintf(numero,"%d",num);
+		
 		//cerramos el descriptor en modo lectura
 		close(df1[0]);
 		
+		//escribimos en el pipe, en este punto se para el proceso hasta que lea el lector
+		write(df1[1],numero,sizeof(numero));
 		
-		//creación del p3 hijo de p1
-		p3=fork();
-		
-		if(p3!=0 && p2=!0){//proceso p1
-			
-			
-		}else{//proceso p3
-			
-			
+		//se queda esperando la señal que mandara el padre
+		while(1){
+			pause();
 		}
 		
 		
-	}else{//proceso p2
+	}else{//proceso p1
 		
+		//cerramos el descriptor para escritura
+		close(df1[1]);
 		
+		//leemos el pipe
+		read(df1[0],numero,sizeof(numero));
 		
+		//convertimos a num
+		num= atoi(numero);
+		
+		printf("Soy el proceso padre p1 y he recibido el numero: %d\n",num);
+		
+		//creación pipe2
+		pipe(df2);
+
+		//creación del p3 hijo de p1
+		p3=fork();
+		
+		if(p3==0){//proceso p3
+			//cerramos el descriptor df1 y el de escritura de df2
+			close(df1[0]);
+			close(df1[1]);
+			close(df2[1]);
+			
+			//recibimos numero
+			read(df2[0],numero,sizeof(numero));
+			
+			//convertimos a num
+			num= atoi(numero);
+			
+			//creamos n lineas por el num recibido
+			for(int i=1;i<=num;i++){
+				printf("%d\n",i);
+			}
+			
+			//se queda esperando la señal que mandara el padre
+			while(1){
+				pause();
+			}
+			
+			
+		}else{
+			//cerramos descriptor de escritura
+			close(df2[0]);
+			
+			//escribirmos en el pipe2 usando la variable char[]
+			write(df2[1],numero,sizeof(numero));
+			
+			//se queda esperando la señal
+			while(1){
+				pause();
+			}
+		}
 	}
-	
-	
-	
-	
-	return 0;
-	}
+}
---
+++ chuleta.c
@@ -0,0 +1,250 @@
+//funciones usadas:
+
+	//- obtener fecha y hora:
+	time_t hora;
+	char *fecha ;
+	time(&hora);
+	fecha = ctime(&hora);
+	
+	//- funciones de cade de caracteres:
+		//- Copiar cadena al lugar que ocupaba otra
+		//si el origen es mas grande que el destino sobreescribiendo memoria
+		strcpy(destino, origen);
+		
+		//-saber el tamaño de una cadena:
+		int tamaño=strlen(cadena);
+		
+		//-comparar cadenas, devolvera 0 si las cadenas son iguales
+		//si la primera es menore devolvera numero negativo y si es mayor positivo
+		int iguales=strcmp(cadena1, cadena2);
+		
+	//-funciones de conversion:
+		//-convierte de cadena a entero
+		int num= atoi(cadena);
+		
+		//-convierte de entero acadena de caracteres:
+		//tambien sirve para unir variables en una cadena
+		//de esta forma unimos en un solo buffer toda la información
+		char cadena[30]= sprintf(buffer_guardar,"cadena con %d,%s,%c",int,char *nombre,char);
+		
+	//-funcion de creación int aleatorio:
+		time_t t;
+		int numero1:
+		srand((unsigned) time(&t));
+		//Generamos numero aleatorio entre 1 y 50
+		numero1=rand() % 50;
+		
+	//-lectura por pantalla:
+		variable  = scanf("%formato",variable);
+		variable  = fgets(variable, MAX_SIZE, stdin);//stdin canal por el que recibe
+													 //max_size-1 siempre (40-1)
+													 //variable de tipo char[]
+		//formatos de tipos:
+			%d	int 	Lee un entero decimal.
+			%f	float 	Lee un número de punto flotante (float).
+			%lf	double Lee un número de punto flotante de doble precisión (double).
+			//todos los anteriores necesitas &
+			%c	char 	Lee un carácter simple.
+			%s	char   (array/puntero)	Lee una cadena de texto, hasta el espacio o salto de linea
+			//estos no
+			scanf(formato(%...),variable del tipo deseado);
+		
+	//-calcular letra de dni:
+		int dni;
+		char letra[] = "TRWAGMYFPDXBNJZSQVHLCKE";
+		dni = “12345678”;
+		dni %= 23;
+		printf("%c",letra[dni]);
+		
+		
+		
+		
+//comandos de consola útiles:
+
+	//- para ver señales: kill -l
+	//- para ver procesos de usuario: ps -u
+	//- para matar pocesos kill -9 pidProceso
+	//- para ver el manual de señales: man 7 signal
+	
+	
+//includes:
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <time.h>
+#include <string.h>
+	
+
+//estructuras de creacion de procesos con control de errores:
+
+	void main(){
+		pid_t p1,p2,p3;
+		
+		p1=getpid();
+		
+		fork(p2);
+		
+		if(p2==-1){ 
+		
+			printf("error al crear p2\n");
+		
+		}else if(p2==0){//bloque p2
+		
+			//codigo que ejecuta p2
+		
+		}else{//bloque p1
+		
+			//codigo que ejecuta el p1
+		
+		}
+	}
+
+//estructura manejador señales:
+
+	void sign_fun(int signal){
+
+		if(signal==SEñAL){
+			if(getpid()==p*){//sirve para usar pid_t globales
+				//codigo que hara al recibir SEñAL
+			}
+		}else if(signal==SEñAL){
+			if(getpid()==p*){//sirve para usar pid_t globales
+				//codigo que hara al recibir SEñAL
+			}
+		}
+
+		//reactivamos las señales
+		signal(SEñAL,sig_fun);
+	}
+	
+	//en el main para capturar cada una de las señales deseadas
+	signal(SEñAL,sign_fun);
+	
+//estructura pipe anonimos:
+//(En los pipes las funciones write y read son bloqueantes)
+//(El mensaje debe ser String o char para enviarlo, si no usaremos la misma refiriendonos al puntero &variable)
+
+	void main{
+		pid_t p1,p2,p3;
+		int df[2];
+		char mensaje[30];//solo si es char[]
+	
+		//creación del pipe
+		pipe(df);
+		
+		p1=getpid();
+		
+		//creación del hijo
+		fork(p2);
+		
+		if(p2==-1){ 
+		
+			printf("error al crear p2\n");
+		
+		}else if(p2==0){//bloque p2
+		
+			//cerrar el descriptor escritor
+			close(df[1]);
+			//leer
+			read(df[0],mensaje,sizeof(mensaje));
+		
+		}else{//bloque p1
+		
+			//cerrar descriptor lector
+			close(df[0]);
+			//escribimos
+			write(df[1],mensaje,sizeof(mensaje));
+		
+		}
+	}
+	
+//estructura pipe FIFO:
+//(tambien se puede usar O_RDWR , esto permitira escribir y leer del mismo FIFO)
+//(IMPORTANTE, aunque no se vaya a leer o escribir, si hemos creado un FIFO debemos
+//hacer que todos los procesos lo abran, si no se bloqueara todo el programa)
+	
+	void main(){
+		const char *fifo1 = "FIFO1";//declaramos el fifo
+		const char *fifo2 = "FIFO2";
+		
+		//creamos el fifo
+		mkfifo(fifo1, 0666);
+		
+		//abrimos el canal
+		int fd1 = open(fifo1, O_WRONLY);
+		
+		if (fd1 == -1) {
+			printf("escritor: error al abrir FIFO");
+			exit(EXIT_FAILURE);
+		}
+		
+		//pasamos la info
+		if (write(fd1, &variable, sizeof(variable)) == -1) {
+			
+			//error al enviar
+			printf("escritor: error al escribir");
+			
+		}	
+		//abrir el fifo2 para poder leer en el receptor
+		int fd2= open(fifo2,O_RDONLY);
+		 if (read(fd2, &variable, sizeof(variable)) == -1) {
+			printf("lector: error al leer");
+			close(fd2);
+			exit(EXIT_FAILURE);
+		}
+		
+		//seguimos con el codigo
+		
+		//para acabar debemos cerrar el descriptor y eliminar el fifo creado
+		close(fd1);
+		unlink("FIFO1");
+		
+	}
+		
+	//codigo de p2 no familiar de p1
+	void main(){
+		const char *fifo1 = "FIFO1";
+		const char *fifo2 = "FIFO2";
+		
+		mkdir ("FIFO2",0666);
+		// Abrir FIFO para lectura (bloquea hasta que el escritor escriba)
+		int fd1 = open(fifo1, O_RDONLY);
+		if (fd1 == -1) {
+			printf("lector: error al abrir FIFO1");
+			exit(EXIT_FAILURE);
+		}
+		
+		// Leer el número enviado
+		if (read(fd1, &variable, sizeof(variable)) == -1) {
+			printf("lector: error al leer");
+			close(fd1);
+			exit(EXIT_FAILURE);
+		}
+		
+		//en este punto el fifo1 a funcionado
+		printf("Número recibido: %d\n", numero);
+		
+		//ahora debemos mandar dicho factorial por el fifo2
+		int fd2= open(fifo2,O_WRONLY);
+		if (write(fd2,&factorial,sizeof(factorial))== -1){
+		
+			printf("escritor: error al escribir");
+			close(fd2);
+			exit(EXIT_FAILURE);
+	
+		}
+		
+		//para acabar debemos cerrar el descriptor y eliminar el fifo creado
+		close(fd2);
+		unlink("FIFO2");
+		
+	}
+		
+		
+		
+		
+	
