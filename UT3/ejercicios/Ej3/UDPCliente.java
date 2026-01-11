2717d4e2ee476bf35c0b1abb039713a919ea7fd1
DiegoBallesteros <diego.ball.fdz@gmail.com>
2026-01-11 14:49:18 +0100

    Ejercicio3 UDP

---
+++ UT3/ejercicios/Ej3/UDPCliente.java
@@ -0,0 +1,65 @@
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class UDPCliente {
+    public static void main(String[] args) {
+        Scanner teclado=new Scanner(System.in);
+        
+        //pedimos los datos
+        System.out.println("Añadiendo una factura nueva:");
+        System.out.println("Introduzca el numero ID: ");
+        String numFact=teclado.nextLine();
+        System.out.println("Introduzca la fecha: ");
+        String fecha=teclado.nextLine();
+        System.out.println("Introduzca el importe: ");
+        double importe=teclado.nextDouble();
+        teclado.nextLine();
+        System.out.println("Introduzca el Tipo de iva (IGC,ESP,EUR): ");
+        String tipoIva=teclado.nextLine();
+        //creamos el obj Factura
+        Factura fac=new Factura(numFact, fecha, importe, tipoIva);
+        
+        //cremos el socket
+        try(DatagramSocket cliente=new DatagramSocket(44444)){
+            InetAddress ip=InetAddress.getByName("192.168.1.41");
+            int port=55555;
+            //preparamos el obj para ser enviado
+            ByteArrayOutputStream bytOut=new ByteArrayOutputStream();
+            ObjectOutputStream salida=new ObjectOutputStream(bytOut);
+            salida.writeObject(fac);
+            salida.flush();//para ordenar enviar ya al Out
+            byte[] bufer=bytOut.toByteArray();//añadimos la info en byte del objeto al array bufer(El que pasaremos)
+            //creamos el flujo de salida
+            DatagramPacket paqueteEnvio=new DatagramPacket(bufer,bufer.length,ip, port);
+            //enviamos
+            cliente.send(paqueteEnvio);
+            System.out.println("Enviado el obj Fact");
+            //recibimos el obj con su importe total e iva calculados
+            //creamos la entrada
+            byte[] bufer2=new byte[1024];
+            DatagramPacket paqueteRecibo=new DatagramPacket(bufer2, bufer2.length);
+            cliente.receive(paqueteRecibo);
+            System.out.println("Recibido el paquete del server");
+            //convertimos a fact
+            ByteArrayInputStream bytIn=new ByteArrayInputStream(paqueteRecibo.getData());//capturamos el bufer con getData();
+            ObjectInputStream entrada=new ObjectInputStream(bytIn);
+            Factura facRecibida=(Factura) entrada.readObject();
+            //imprimos la factura
+            System.out.println("Factura recibida.");
+            System.out.println(facRecibida.toString());
+            cliente.close();
+
+        }catch(Exception e){
+            System.out.println("Algo salio mal con el cliente: "+e.getMessage());
+            e.printStackTrace();
+        }
+    }
+    
+}
---
+++ UT3/ejercicios/Ej3/UDPServidor.java
@@ -0,0 +1,50 @@
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class UDPServidor {
+    public static void main(String[] args) {
+        //creacion del socket
+        try(DatagramSocket server=new DatagramSocket(55555)){
+            System.out.println("Servidor a la escucha de paquetes....");
+            //creamos el flujo de entrada
+            byte[] bufer=new byte[1024];
+            DatagramPacket paqueteRecibido=new DatagramPacket(bufer, bufer.length);
+            //recibimos el paquete
+            server.receive(paqueteRecibido);
+            //convertimos el obj
+            ByteArrayInputStream bytIn=new ByteArrayInputStream(paqueteRecibido.getData());
+            ObjectInputStream entrada=new ObjectInputStream(bytIn);
+            Factura facRecibida=(Factura) entrada.readObject();
+            System.out.println("Factura recibida");
+            //hacemos los calculos
+            facRecibida.calcularImporteTotal();
+            System.out.println("Calculos realizados.");
+            System.out.println(facRecibida.toString());
+
+            //preparamos el enivio
+            ByteArrayOutputStream bytOut=new ByteArrayOutputStream();
+            ObjectOutputStream salida=new ObjectOutputStream(bytOut);
+            byte[] bufer2=bytOut.toByteArray();
+            salida.writeObject(bufer2);
+            salida.flush();
+            //enviar el objeto
+            InetAddress ip= paqueteRecibido.getAddress();
+            int port=paqueteRecibido.getPort();
+            DatagramPacket paqueteEnviar=new DatagramPacket(bufer2, bufer2.length,ip,port);
+            server.send(paqueteEnviar);
+            System.out.println("El servidor a acabado su trabajo");
+            server.close();          
+        }catch(Exception eServer){
+            System.out.println("Algo salio mal con el servidor: "+eServer.getMessage());
+            eServer.printStackTrace();
+        }
+    }
+}
