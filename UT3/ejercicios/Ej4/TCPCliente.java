560f902c5e67b7e33b7322bf9ddba47a6c886efd
DiegoBallesteros <diego.ball.fdz@gmail.com>
2026-01-12 22:16:18 +0100

    Ejercicio4

--- UT3/ejercicios/Ej4/TCPCliente.java
+++ UT3/ejercicios/Ej4/TCPCliente.java
@@ -1,17 +1,47 @@
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.net.SocketException;
+import java.util.Scanner;
 
 public class TCPCliente {
     public static void main(String[] args) {
+        boolean salir=false;
+        int cont=1;
+        Scanner teclado=new Scanner(System.in);
+        int pid;
+        String mensaje;
         try(Socket cliente=new Socket(InetAddress.getByName("192.168.1.41"),55555)){
             //creamos el flujo de salida
             ObjectOutputStream flujoSalida=new ObjectOutputStream(cliente.getOutputStream());
-            //preparamos el obj para ser enviado
-            flujoSalida.writeObject("Hola Server");
-            flujoSalida.flush();//ordenamos que se envie y vacio el bufferStream
-            System.out.println("Enviado el obj");
+            ObjectInputStream flujoEntrada=new ObjectInputStream(cliente.getInputStream());
+            //preparamos el bucle del juego
+            while(!salir){
+                //solicitamos el numero para adivinar
+                System.out.println("Ronda "+cont+": \n \t Indique el número que cree correcto:");
+                pid=teclado.nextInt();
+                //enviamos al server
+                flujoSalida.writeObject(pid);
+                flujoSalida.flush();
+                //recibimos el mensaje
+                mensaje=String.valueOf(flujoEntrada.readObject());
+                //verificamos que acaba el string con ganado.
+                if(mensaje.endsWith("ganado.")){
+                    //paramos el bucle
+                    salir=true;
+                }
+                //mostramos el mensaje
+                System.out.println(mensaje);
+                //subimos la ronda
+                cont++;
+            }
             cliente.close();
+        }catch(SocketException eSocket){
+            System.out.println("Usted a perdido");
+        }catch(Exception e){
+            System.out.println("Algo salio mal en el cliente: "+e.getMessage());
+            e.printStackTrace();
         }
     }
     
